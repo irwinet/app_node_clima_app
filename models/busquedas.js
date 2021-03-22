719e21d4a7c6b83fc157ab8a67532f9323f0f05a
Irwin <irwinestradatorres@gmail.com>
2021-03-22 10:10:54 -0500

    Create Instance Axios

--- models/busquedas.js
+++ models/busquedas.js
@@ -7,12 +7,27 @@ class Busquedas {
         // TODO: leer DB si existe
     }
 
+    get paramsMapbox() {
+        return {
+            'access_token': 'pk.eyJ1IjoiaXJ3aW5ldCIsImEiOiJja21rcG00bHYxM2FkMnZuMDc2Y21sdmk0In0.mZH7qpjV8NmWerLnh2675g',
+            'limit': 5,
+            'language': 'es'
+        };
+    }
+
     async ciudad(lugar = '') {
         try {
             // peticion http
             // console.log(lugar);
 
-            const resp = await axios.get('https://reqres.in/api/users?page=2');
+            const instance = axios.create({
+                baseURL: `https://api.mapbox.com/geocoding/v5/mapbox.places/${lugar}.json`,
+                params: this.paramsMapbox
+            });
+
+            const resp = await instance.get();
+
+            // const resp = await axios.get('https://api.mapbox.com/geocoding/v5/mapbox.places/Madrid.json?access_token=pk.eyJ1IjoiaXJ3aW5ldCIsImEiOiJja21rcG00bHYxM2FkMnZuMDc2Y21sdmk0In0.mZH7qpjV8NmWerLnh2675g&limit=5&language=es');
             console.log(resp.data);
 
             return []; //retornar los lugares
